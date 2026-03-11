959dcbcd61a9de6e5dc4fa0082acd98502c0ee78
xyriltumbali-Pupians <xyriltumbali@gmail.com>
2026-03-11 11:42:06 +0800

    Name_print_no_space

---
+++ input_your_name_with_space.py
@@ -0,0 +1,6 @@
+# name = input("Please type your name here")
+# print(name)
+
+fullname = input("Enter your full name (with spaces at the beginning): ")
+clean_name = fullname.strip()
+print("Full name without leading spaces:", clean_name)
