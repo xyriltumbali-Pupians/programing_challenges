cdd57ff0f492ca85bd545aebc3540f89766aea3c
xyriltumbali-Pupians <xyriltumbali@gmail.com>
2026-03-18 19:21:06 +0800

    print_equal_if_same

---
+++ print_equal_same_value.py
@@ -0,0 +1,10 @@
+num1  = int(input("please input number:"))
+num2 =int(input("pleass input same value or number from the first:"))
+
+if num1 == num2:
+    print("the value are equal" , num1,num2 )
+    
+else:
+    print("values are not equal, try to put again")
+    
+    
