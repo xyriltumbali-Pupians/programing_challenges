4176a638b75a163f75f9bacb12186af5796134bd
xyriltumbali-Pupians <xyriltumbali@gmail.com>
2026-03-18 19:21:54 +0800

    show_the_bigger_value

---
+++ print_bigger_number.py
@@ -0,0 +1,7 @@
+num1 = int(input("please input 1st value:"))
+num2 = int(input("please input 2nd value:"))
+
+if num1 > num2:
+    print(f"the fist value you enter is bigger" , num1)
+else:
+    print(f"the second value you enter is bigger" , num2)
