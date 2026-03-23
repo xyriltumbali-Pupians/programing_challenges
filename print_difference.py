94c02b7c2a403588cd3928db3ffc1902929c041d
xyriltumbali-Pupians <xyriltumbali@gmail.com>
2026-03-23 21:54:10 +0800

    the_difference

---
+++ print_difference.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+dirrefence = num1 - num2
+print("The difference between the two numbers is:", dirrefence)
